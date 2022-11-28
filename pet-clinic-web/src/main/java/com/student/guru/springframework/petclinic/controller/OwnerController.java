d395144c184edc80684fe6fb7ae4f041f4457d11
NathanSoa <nathansoares809@gmail.com>
2022-11-28 11:26:39 -0300

    add list of owner on html file

--- pet-clinic-web/src/main/java/com/student/guru/springframework/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/student/guru/springframework/petclinic/controller/OwnerController.java
@@ -1,13 +1,23 @@
 package com.student.guru.springframework.petclinic.controller;
 
+import com.student.guru.springframework.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owner")
 @Controller
 public class OwnerController {
 
-    public String listOwner(){
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/"})
+    public String listOwner(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
