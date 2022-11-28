51122a4d13f5fd71debe9d9b95de24d7e3d60284
NathanSoa <nathansoares809@gmail.com>
2022-11-28 11:35:41 -0300

    add list of vets on html file

--- pet-clinic-web/src/main/java/com/student/guru/springframework/petclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/com/student/guru/springframework/petclinic/controller/VetsController.java
@@ -1,14 +1,23 @@
 package com.student.guru.springframework.petclinic.controller;
 
+import com.student.guru.springframework.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetsController {
 
-    @RequestMapping("/vets")
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
