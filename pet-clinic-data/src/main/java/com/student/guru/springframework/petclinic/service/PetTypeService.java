b440cfda21617af0b94cfddebc497f747cba0cc9
NathanSoa <nathansoares809@gmail.com>
2022-12-06 21:22:11 -0300

    add: pet type service and map implementation

---
+++ pet-clinic-data/src/main/java/com/student/guru/springframework/petclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.student.guru.springframework.petclinic.service;
+
+import com.student.guru.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
