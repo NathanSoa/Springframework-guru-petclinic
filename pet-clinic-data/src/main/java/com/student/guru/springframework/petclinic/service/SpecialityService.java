2ab5ce60d4ac741bf8e4fe0811e31118b3fb5376
NathanSoa <nathansoares809@gmail.com>
2022-12-07 09:27:21 -0300

    add: speciality service interface and map implementation

---
+++ pet-clinic-data/src/main/java/com/student/guru/springframework/petclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.student.guru.springframework.petclinic.service;
+
+import com.student.guru.springframework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/student/guru/springframework/petclinic/service/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package com.student.guru.springframework.petclinic.service.map;
+
+import com.student.guru.springframework.petclinic.model.Speciality;
+import com.student.guru.springframework.petclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
