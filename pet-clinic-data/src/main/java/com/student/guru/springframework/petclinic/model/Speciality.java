8745e37493a010e0fa5deb7d7e0afd8f082d6ef7
NathanSoa <nathansoares809@gmail.com>
2022-12-06 21:15:58 -0300

    add: vet speciality entity

---
+++ pet-clinic-data/src/main/java/com/student/guru/springframework/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.student.guru.springframework.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
