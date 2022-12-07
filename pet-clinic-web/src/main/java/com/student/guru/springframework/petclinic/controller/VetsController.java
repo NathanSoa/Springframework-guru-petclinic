a91f8a1ad9af9f184edac547279c5c2a41a9a791
NathanSoa <nathansoares809@gmail.com>
2022-12-07 13:20:31 -0300

    refactor: add mapping for pages that aren't implemented

--- pet-clinic-web/src/main/java/com/student/guru/springframework/petclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/com/student/guru/springframework/petclinic/controller/VetsController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
 public class VetsController {
 
     private final VetService vetService;
@@ -15,7 +14,7 @@ public class VetsController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/"})
+    @RequestMapping({"/vets", "/vets/", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
